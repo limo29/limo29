2376ab9fd194ddf2fdc59c498cd3e99ee0c7aa5b
limo29 <61993402+limo29@users.noreply.github.com>
2020-03-10 00:46:20 +0100

    Add files via upload

--- Javascript_Aufgaben.js
+++ Javascript_Aufgaben.js
@@ -5,26 +5,25 @@ var aufgaben =[
  "Trink 10 Schlücke",
  "Trink 5 Schlücke",
  "Trink 3 Schlücke",
- "Trink 2 Schlücke",
- "Trink 2 Schlücke",
- "Alle die gestern besoffen waren trinken 5 Schlücke",
- "Der jüngste muss sein Glas exen",
- "Der älteste muss sein Glas exen"
- "Alle auf ex",
+  "Trink 2 Schlücke",
+  "Trink 2 Schlücke",
  "Verteil 5 Schlücke",
  "Verteil 4 Schlücke",
  "Verteil 3 Schlücke",
  "Trink!",
  "Straf - Schnaps",
+ "Alle die gestern besoffen waren trinken 5 Schlücke",
  "2 Schlücke, wenn du schon einmal bei Google nach 2 girls, 1 cup gesucht hast",
  "Alle Nicht-Vegetarier trinken 5 Schlücke",
  "4 Schlücke, wenn du schonmal so tun musstest, als ob du nüchtern bist obwohl du sternhagel voll warst",
  "2 Schlücke, wenn du schonmal in aller Öffentlichkeit gekotzt hast",
+ "Alle auf ex",
  "Alle singles trinken 3 Schlücke",
  "Alle Frauen trinken 5 Schlücke",
  "Alle Frauen trinken 5 Schlücke",
  "Trinkbuddy, immer doppelt soviel wie vorgegeben",
- 
+ "Der jüngste muss sein Glas exen",
+ "Der älteste muss sein Glas exen"
 ]
 var timeLeft = -1;
 var lastNumber = 0;
@@ -40,15 +39,6 @@ function newAufgabe(){
 	if(randomNumber == 0){timeLeft= 60;}
 	if(randomNumber == 1){timeLeft= 60;}
 	if(randomNumber == 2){timeLeft= 120;}
-	if(randomNumber == 3){timeLeft= 30;}
-	if(randomNumber == 4){timeLeft= 20;}
-	if(randomNumber == 5){timeLeft= 8;}
-	if(randomNumber == 6){timeLeft= 8;}
-	if(randomNumber == 7){timeLeft= 8;}
-	if(randomNumber == 8){timeLeft= 20;}
-	if(randomNumber == 9){timeLeft= 60;}
-	if(randomNumber == 10){timeLeft= 60;}
-	if(randomNumber == 11){timeLeft= 60;}
 	}
 }
 
